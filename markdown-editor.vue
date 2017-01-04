bf8b68089d6d4a04668281ea05be7b5b1e591f13
- - <945852046@qq.com>
2017-01-04 17:56:04 +0800

    add highlight&theme

--- markdown-editor.vue
+++ markdown-editor.vue
@@ -12,6 +12,7 @@ export default {
   name: 'markdown-editor',
   props: {
     value: String,
+    previewClass: String,
     configs: {
       type: Object
     }
@@ -25,14 +26,44 @@ export default {
       configs.element = configs.element || this.$el.firstChild
       configs.initialValue = configs.initialValue || this.value
 
+      // 开启代码高亮
+      if (configs.renderingConfig.codeSyntaxHighlighting) {
+        require.ensure([], () => {
+          var theme = configs.renderingConfig.highlightingTheme || 'default'
+          window.hljs = require('highlight.js')
+          require('highlight.js/styles/' + theme + '.css')
+        }, 'highlight')
+      }
+
       // 实例化编辑器
       this.simplemde = new SimpleMDE(configs)
+
+      // 添加自定义 previewClass
+      this.addPreviewClass()
 
       // 绑定输入事件
       this.simplemde.codemirror.on('change', () => {
         this.$emit('input', this.simplemde.value())
       })
+    },
+    addPreviewClass () {
+      var _class = this.configs.renderingConfig.codeSyntaxHighlighting ? this.previewClass + ' hljs' : this.previewClass
+      var wrapper = this.simplemde.codemirror.getWrapperElement()
+      var preview = document.createElement('div')
+      this.$el.lastChild.className += ' ' + _class
+      preview.className = 'editor-preview ' + _class
+      wrapper.appendChild(preview)
     }
   }
 }
 </script>
+
+<style>
+.markdown-editor .hljs {
+  display: none
+}
+
+.markdown-editor .editor-preview-active, .markdown-editor .editor-preview-active-side {
+  display: block;
+}
+</style>
