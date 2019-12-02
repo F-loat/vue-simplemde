0a28d6215ce3ebe3c25046189cf54f90fc414196
F-loat <chaimaoyuan@foxmail.com>
2019-12-02 10:19:16 +0800

    fix: #71 toggleFullScreen with keep-alive

--- src/index.vue
+++ src/index.vue
@@ -42,11 +42,11 @@ export default {
   mounted() {
     if (this.autoinit) this.initialize();
   },
-  activated() {
+  deactivated() {
     const editor = this.simplemde;
     if (!editor) return;
-    const isActive = editor.isSideBySideActive() || editor.isPreviewActive();
-    if (isActive) editor.toggleFullScreen();
+    const isFullScreen = editor.codemirror.getOption('fullScreen');
+    if (isFullScreen) editor.toggleFullScreen();
   },
   methods: {
     initialize() {
