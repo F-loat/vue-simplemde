fe44925fea2f4786a079040e1d3e58a00cbb395f
F-loat <chaimaoyuan@foxmail.com>
2022-01-11 11:00:18 +0800

    chore: add resolutions for marked

--- hack.js
+++ hack.js
@@ -5,7 +5,7 @@ const html = fs.readFileSync('dist/demo.html').toString();
 const hackedHtml = html.replace('<script src="https://unpkg.com/vue"></script>', `
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
 <script src="https://unpkg.com/vue"></script>
-<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/marked@3.0.8/marked.min.js"></script>
 <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
 `);
 
